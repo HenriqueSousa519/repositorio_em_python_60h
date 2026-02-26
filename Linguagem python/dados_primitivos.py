e214c2e1fa7d28de7ffd1546138772b77c89ee6f
HenriqueSousa519 <riquesousa362@gmail.com>
2026-02-26 17:24:25 -0300

    Add files via upload

---
+++ Linguagem python/dados_primitivos.py
@@ -0,0 +1,25 @@
+# dados primitivos em python
+print('olá mundo')
+
+# tipo string - texto
+'texto'
+"texto 2 fdahhfskjabfjskabkj"
+
+# tipo interger - inteiro
+print (1+2)
+1
+2
+100
+
+# tipo float - real
+print (5.0 - 100.5)
+5.0
+2.10
+100.
+0.5
+
+# tipo bool - lógico
+
+True
+False
+
