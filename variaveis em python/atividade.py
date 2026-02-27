e3121c3825b1da0f1ffc0b612beb59d5583ce9bd
HenriqueSousa519 <riquesousa362@gmail.com>
2026-02-27 17:11:56 -0300

    Add files via upload

---
+++ variaveis em python/atividade.py
@@ -0,0 +1,23 @@
+print('atividade 1')
+n1 = 20
+print(n1 * 2)
+print('---'* 15)
+print('atividade 2')
+nome = 'Henrique'
+sobrenome = 'Sousa'
+print (nome ,sobrenome)
+print('---'* 15)
+print('atividade 3')
+n2 = float(input('digite um numero inteiro:'))
+n3 = float(input('digite um numero inteiro:'))
+print(n2 , n3)
+print('---'* 15)
+print('atividade 4')
+palavra = 'python'
+print(palavra, n1)
+print('---'* 15)
+print('atividade 5')
+frase = 'que dia da semana é hoje? '
+palavra2 = input('digite o dia da semana de hoje por extenso:')
+print (frase, palavra2)
+print('---'* 15)
