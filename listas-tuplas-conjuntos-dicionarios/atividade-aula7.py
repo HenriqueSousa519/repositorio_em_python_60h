22f26a9976118759a33609deca3f2ff5d3a3728a
HenriqueSousa519 <riquesousa362@gmail.com>
2026-03-05 17:55:05 -0300

    Add files via upload

---
+++ listas-tuplas-conjuntos-dicionarios/atividade-aula7.py
@@ -0,0 +1,13 @@
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+print(numeros)
+print('---'* 15)
+print(numeros[2])
+print('---'* 15)
+numeros.append(9)
+print(numeros)
+print('---'* 15)
+numeros.remove(5)
+print(numeros)
+print('---'* 15)
+carros = ['R33', 'R34', 'R35']
+print (numeros, carros)
---
+++ listas-tuplas-conjuntos-dicionarios/listas.py
@@ -0,0 +1,14 @@
+#lista inteiros
+lista_inteiros = [1,2,5]
+# decimal
+lista_decimal = [1.6,2.9,3.5]
+#textos
+lista_texto = ['teste', 'teste 2']
+#booleana
+#lista_booleana=[true, false]
+#range/list
+lista = [1,2,3,4,5]
+print(lista)
+
+l = list(range(7,25,3))
+print(l)
