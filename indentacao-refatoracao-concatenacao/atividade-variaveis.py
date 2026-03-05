bda8fff66421f96661d81071b0f466627f06844b
HenriqueSousa519 <riquesousa362@gmail.com>
2026-03-05 15:54:29 -0300

    Add files via upload

---
+++ indentacao-refatoracao-concatenacao/atividade-variaveis.py
@@ -0,0 +1,48 @@
+md = (4*6) / 2
+print ('o resultado da conta 4*6/2 é ', md)
+print('___'*15)
+soma = (5+7)
+print ('o resultado da soma de 5+7 é ', soma)
+print('___'*15)
+eleve = (3**4)
+print ('o resultado de elevar 3 por 4 é ', eleve)
+print('___'*15)
+media = (8 + 12 + 15) / 3
+print('a media de 8, 12 3 15 é ', media)
+print('___'*15)
+sub = (2-10)*5
+print('o resultado da subtração 2-10 é ', sub)
+print('___'*15)
+div = (27/3) + 5
+print('o resultado da conta 27/3+5 é ', div)
+print('___'*15)
+modulo = (17%4)
+print('o resto da divisão 17 por 4 é', modulo)
+print('___'*15)
+mult = (9*3)**2
+print ('o resultado da multiplicação é', mult)
+print('___'*15)
+raiz = ( 81**0.5)
+print('a raiz de 81 é ', raiz)
+print('___'*15)
+adic = (3*4) + 20
+print('o resultado disso é ', adic)
+print('___'*15)
+multi = (15*2) - 7
+print (' o resultado vai ser ', multi)
+print('___'*15)
+elevee = (5**3)
+print(' o cubo de 5 é ', elevee)
+print('___'*15)
+mediaa = (17 + 21 + 25)/ 3
+print ('a media vai ser ', mediaa)
+print('___'*15)
+dnv = (11*2) + 7
+print ('a conta vai dar ', dnv)
+print('___'*15)
+subi = ((3*8)- 15) / 2
+print (' o resultado é ', subi)
+print('___'*15)
+pot = (2**10)
+print ('a potencia vai ser ', pot)
+print('___'*15)
---
+++ indentacao-refatoracao-concatenacao/variaveis.py
@@ -0,0 +1,55 @@
+print ('olá, Maria')
+
+# sintxe - gramatical
+# refatoração
+# metodos - funções
+
+# palavra () - ação - função
+
+#print()
+#input()
+
+palavra = "variavel"
+variaveis = 10
+listas = [1,2,3]
+tuplas = (1,2,3)
+conjuntos = {1,2,3}
+dicionario = ()
+
+# estrutura de dados - objeto 
+print ('palavra')
+# ______________________________________________________________________________________________________
+#str()
+#float()
+#int()
+#bool()
+#list()
+#len(objeto) 
+#max()
+#min()
+#tuple()
+#sorted()
+#sum()
+#---------------------------------------------------------------------------------------------
+objeto = 'texto'
+
+print(len(objeto)) #tamanho
+x = (max(objeto))
+print (x)
+print (min(objeto))
+
+text = 'isso é um texto'
+print(list(text)) # lista - mutavel
+print(tuple(text)) # tupla - imutavel
+
+#x = sorted(text) #ordenação
+#text.sort() #metodo de objeto
+#x = {1,2,3}
+#print(dir(x))
+
+x = 10
+y = 5
+
+#print(sum([1,2,3]))
+#r = list(range(11))
+#print(r)
